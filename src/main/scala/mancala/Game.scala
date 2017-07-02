5b7be6c3413c06d71554fa894922a0f662c5d65a
Andrii Tsymbala <atsymbala@linkedin.com>
2017-07-01 22:43:23 -0700

    Reshuffle methods to bring public props to the top

--- src/main/scala/mancala/Game.scala
+++ src/main/scala/mancala/Game.scala
@@ -11,8 +11,6 @@ class Game private (val board: List[Int], val moves: List[Int], val bottomNext: 
 
   def ended = top == rowOf(0) || bottom == rowOf(0)
 
-  private[this] def rowOf(x: Int): List[Int] = List.fill(size){ x }
-
   override def toString: String = {
     val movesRow = moves.reverse.mkString(" ")
     val topRow = top.reverse.mkString("\t")
@@ -20,20 +18,76 @@ class Game private (val board: List[Int], val moves: List[Int], val bottomNext: 
     s"$movesRow\n[ $topMancala ]\t$topRow\n\t$bottomRow\t[ $bottomMancala ]"
   }
 
-  private[this] def turnBoard(b: List[Int]): List[Int] = {
-    b.slice(size + 1, lastIndex + 1) ++ b.slice(0, size + 1)
-  }
-
   def getStones(cell: Int): Either[String, Int] = {
     if (cell > size || cell < 1) Left(s"Invalid cell #. Only numbers from 1 to $size are allowed.")
     else Right(if (bottomNext) bottom(cell - 1) else top(cell - 1))
   }
 
-  // TODO: weird helper method. Can we get rid of it?
-  private[this] def cyclesAndTail(stones: Int): (Int, Int) = {
-    val cycles = stones / lastIndex
-    val tail = stones - cycles * lastIndex
-    (cycles, tail)
+  def move(cell: Int): Either[String, Game] = {
+    for {
+      stones <- getStones(cell)
+      game <- movedGame(cell, stones)
+    } yield game
+  }
+
+  private[this] def updateBoard(cell: Int, stones: Int): List[Int] = {
+    def turnBoard(b: List[Int]): List[Int] = {
+      b.slice(size + 1, lastIndex + 1) ++ b.slice(0, size + 1)
+    }
+    val activeBoard = if (bottomNext) board else turnBoard(board)
+    val updatedBoard = reflectMove(activeBoard, cell, stones)
+    val conqueredBoard = conquering(cell, stones) match {
+      case Some(index) => {
+        reflectConquer(updatedBoard, index)
+      }
+      case None => updatedBoard
+    }
+    if (bottomNext) conqueredBoard else turnBoard(conqueredBoard)
+  }
+
+  private[this] def movedGame(cell: Int, stones: Int): Either[String, Game] = {
+    def endsInMancala(cell: Int, stones: Int): Boolean = {
+      val (_, tail) = cyclesAndTail(stones)
+      tail == size + 1 - cell
+    }
+    if (stones == 0) {
+      Left(s"Invalid move. Cell #$cell is empty.")
+    } else {
+      val newBoard = updateBoard(cell, stones)
+      val newBottomNext = if (endsInMancala(cell, stones)) bottomNext else !bottomNext
+      val newMoves = cell :: moves
+      val game = new Game(newBoard, newMoves, newBottomNext)
+      if (game.ended) {
+        val topScore = game.topMancala + game.top.fold(0)(_ + _)
+        val bottomScore = game.bottomMancala + game.bottom.fold(0)(_ + _)
+        Right(new Game(rowOf(0) ++ List(bottomScore) ++ rowOf(0) ++ List(topScore), newMoves, bottomNext))
+      } else {
+        Right(game)
+      }
+    }
+  }
+
+  private[this] def reflectMove(board: List[Int], cell: Int, stones: Int): List[Int] = {
+    val (cycles, tail) = cyclesAndTail(stones)
+    board.zipWithIndex.map {
+      case (stonesInCell, cellIndex) => {
+        if (cellIndex == lastIndex) { // Don't add stones to oposite mancala
+           stonesInCell
+        } else {
+          if (cellIndex == cell - 1) {
+            cycles
+          } else {
+            val index = if (cellIndex < cell - 1) {
+              cellIndex + lastIndex
+            } else {
+              cellIndex
+            }
+            val extraStone = if (tail > index - cell) 1 else 0
+            stonesInCell + cycles + extraStone
+          }
+        }
+      }
+    }
   }
 
   private[this] def conquering(cell: Int, stones: Int): Option[Int] = {
@@ -62,70 +116,14 @@ class Game private (val board: List[Int], val moves: List[Int], val bottomNext: 
     bottom ++ top
   }
 
-  private[this] def reflectMove(board: List[Int], cell: Int, stones: Int): List[Int] = {
-    val (cycles, tail) = cyclesAndTail(stones)
-    board.zipWithIndex.map {
-      case (stonesInCell, cellIndex) => {
-        if (cellIndex == lastIndex) { // Don't add stones to oposite mancala
-           stonesInCell
-        } else {
-          if (cellIndex == cell - 1) {
-            cycles
-          } else {
-            val index = if (cellIndex < cell - 1) {
-              cellIndex + lastIndex
-            } else {
-              cellIndex
-            }
-            val extraStone = if (tail > index - cell) 1 else 0
-            stonesInCell + cycles + extraStone
-          }
-        }
-      }
-    }
+  // TODO: weird helper method. Can we get rid of it?
+  private[this] def cyclesAndTail(stones: Int): (Int, Int) = {
+    val cycles = stones / lastIndex
+    val tail = stones - cycles * lastIndex
+    (cycles, tail)
   }
 
-  private[this] def updateBoard(cell: Int, stones: Int): List[Int] = {
-    val activeBoard = if (bottomNext) board else turnBoard(board)
-    val updatedBoard = reflectMove(activeBoard, cell, stones)
-    val conqueredBoard = conquering(cell, stones) match {
-      case Some(index) => {
-        reflectConquer(updatedBoard, index)
-      }
-      case None => updatedBoard
-    }
-    if (bottomNext) conqueredBoard else turnBoard(conqueredBoard)
-  }
-
-  private[this] def endsInMancala(cell: Int, stones: Int): Boolean = {
-    val (_, tail) = cyclesAndTail(stones)
-    tail == size + 1 - cell
-  }
-
-  private[this] def movedGame(cell: Int, stones: Int): Either[String, Game] = {
-    if (stones == 0) {
-      Left(s"Invalid move. Cell #$cell is empty.")
-    } else {
-      val newBoard = updateBoard(cell, stones)
-      val newBottomNext = if (endsInMancala(cell, stones)) bottomNext else !bottomNext
-      val newMoves = cell :: moves
-      val game = new Game(newBoard, newMoves, newBottomNext)
-      if (game.ended) {
-        val topScore = game.topMancala + game.top.fold(0)(_ + _)
-        val bottomScore = game.bottomMancala + game.bottom.fold(0)(_ + _)
-        Right(new Game(rowOf(0) ++ List(bottomScore) ++ rowOf(0) ++ List(topScore), newMoves, bottomNext))
-      } else {
-        Right(game)
-      }
-    }
-  }
-
-  def move(cell: Int): Either[String, Game] = {
-    for {
-      stones <- getStones(cell)
-      game <- movedGame(cell, stones)
-    } yield game
-  }
+  private[this] def rowOf(x: Int): List[Int] = List.fill(size){ x }
 }
 
 
